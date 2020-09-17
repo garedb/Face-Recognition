800610810ddebd6b13ed0ef69945fe968570a68d
garedb <garedbarney@gmail.com>
2020-09-17 11:56:29 -0700

    completed front-end with signup/ signin/ and register form

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,9 @@ const Logo = () => {
   return (
     <div className='ma4 mt0'>
       <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 100, width: 100 }} >
-        <div className="Tilt-inner pa3"><img style={{paddingTop: '2px'}} alt='logo' src={face}/></div>
+        <div className="Tilt-inner pa3">
+          <img style={{paddingTop: '2px'}} alt='logo' src={face}/>
+        </div>
       </Tilt>
     </div>
   );
